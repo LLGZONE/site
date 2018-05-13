f8c5e2bf5e00baa88a10f044b8239b55bb55ad7f
hardfist <1562502418@qq.com>
2018-05-13 23:52:59 +0800

    support pm2
    Change-Id: I79563460a6b77ec39bb5cbd4d3fc98e002d7493c

--- router/index.js
+++ router/index.js
@@ -4,5 +4,8 @@ const router = new Router();
 router.get('/hello', async (ctx,next) => {
   ctx.body = 'hello';
 })
+router.post('/hooks', async (ctx,next) => {
+  console.log('hook info:', ctx.body);
+});
 
 module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
 const router = require('../router');
+const koaBody = require('koa-body');
 const config = require('../config/conf.local');
 
 const app = new Koa();
+app.use(koaBody());
 app.use(async (ctx,next) => {
   ctx.config = config;
   await next();
