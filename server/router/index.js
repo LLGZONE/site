624e50622b7228cda23599ed993d9784b1c6ce3b
hardfist <1562502418@qq.com>
2018-05-19 16:16:34 +0800

    route simplify
    Change-Id: I37073995d25c88d8743970bb5aff6309588ad622

--- server/router/index.js
+++ server/router/index.js
@@ -2,13 +2,13 @@ const Router = require('koa-router');
 const router = new Router();
 const indexController = require('../controller/indexController');
 const userController = require('../controller/userController');
+router.prefix('/api');
+router.get('/view', indexController.index);
 
-router.get('/api/view', indexController.index);
-
-router.get('/api/user/get', userController.user_info);
-router.post('/api/signin', userController.signin);
-router.post('/api/signup', userController.signup);
-router.post('/api/signout', userController.signout);
+router.get('/user/get', userController.user_info);
+router.post('/signin', userController.signin);
+router.post('/signup', userController.signup);
+router.post('/signout', userController.signout);
 
 router.stack.forEach(element => {
   console.log(element.path, element.methods);
