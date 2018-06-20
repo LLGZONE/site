a3cfc3632ed0684f3e56b05717c96e6e1b83df33
hardfist <1562502418@qq.com>
2018-06-21 00:08:33 +0800

    update config

---
+++ server/.babelrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/stage-0',
+    '@babel/typescript'
+  ]
+};
--- server/db/index.js
+++ server/db/index.js
@@ -2,36 +2,41 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const config = require('../config');
-const client = new Sequelize(config.db.database, config.db.username, config.db.password, {
-  host: config.db.host,
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-});
+const client = new Sequelize(
+  config.db.database,
+  config.db.username,
+  config.db.password,
+  {
+    host: config.db.host,
+    logging: false,
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
 var models = {};
 
 // read all models and import them into the "db" object
-fs
-    .readdirSync(__dirname + '/models')
-    .filter(function (file) {
-        return (file.indexOf('.') !== 0) && (file !== 'index.js');
-    })
-    .forEach(function (file) {
-        var model = client.import(path.join(__dirname + '/models', file));
-        models[model.name] = model;
-    });
+fs.readdirSync(__dirname + '/models')
+  .filter(function(file) {
+    return file.indexOf('.') !== 0 && file !== 'index.js';
+  })
+  .forEach(function(file) {
+    var model = client.import(path.join(__dirname + '/models', file));
+    models[model.name] = model;
+  });
 
-Object.keys(models).forEach(function (modelName) {
-    if (models[modelName].options.hasOwnProperty('associate')) {
-        models[modelName].options.associate(models);
-    }
+Object.keys(models).forEach(function(modelName) {
+  if (models[modelName].options.hasOwnProperty('associate')) {
+    models[modelName].options.associate(models);
+  }
 });
 
 module.exports = {
   models,
   client
-}
+};
