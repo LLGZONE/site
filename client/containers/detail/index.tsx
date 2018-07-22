10081ba0791e478bfc28c17c885938393d0af179
hardfist <1562502418@qq.com>
2018-07-22 22:25:48 +0800

    文案实例
    Change-Id: I9c665b0ae83cdf4eede213765426d90ebe389e1b

--- client/containers/detail/index.tsx
+++ client/containers/detail/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import http from 'lib/http';
 import * as URL from 'constants/api/topfeed';
-import Nav from '../../components/nav';
+import Layout from 'components/layout';
+import intl from 'react-intl-universal';
 
 export default class Detail extends React.Component<{
   item_id: string;
@@ -30,6 +31,50 @@ export default class Detail extends React.Component<{
     }
   }
   render() {
-    return <Nav />;
+    let start = new Date();
+    let end = new Date();
+    let expires = new Date();
+    let price = 123456.78;
+    return (
+      <Layout>
+        <h1>基本使用</h1>
+        <div>
+          <div className="title">Basic Examples:</div>
+          <div>{intl.get('SIMPLE')}</div>
+          <div>{intl.get('HELLO', { name: 'Tony', where: 'Alibaba' })}</div>
+        </div>
+        <h1>日期显示</h1>
+        <div className="title">Date Examples:</div>
+        <div>{intl.get('SALE_START', { start })}</div>
+        <div>{intl.get('SALE_END', { end })}</div>
+        <div>{intl.get('COUPON', { expires })}</div>
+        <h1>货币显示</h1>
+        <div>
+          <div className="title">Currency Example:</div>
+          <div>{intl.get('SALE_PRICE', { price })}</div>
+        </div>
+        <h1>单复数</h1>
+        <div>
+          <div className="title">Plural Examples:</div>
+          <div>{intl.get('PHOTO', { photoNum: 0 })}</div>
+          <div>{intl.get('PHOTO', { photoNum: 1 })}</div>
+          <div>{intl.get('PHOTO', { photoNum: 1000000 })}</div>
+        </div>
+        <h1>富文本</h1>
+        <div>
+          <div className="title">Html Examples:</div>
+          <div>{intl.getHTML('TIP')}</div>
+          <div>
+            {intl.getHTML('TIP_VAR', { message: 'HTML with variables' })}
+          </div>
+          <div>
+            {intl.getHTML('TIP_VAR', {
+              message:
+                '<script>alert("ReactIntlUniversal prevents from xss attack")</script>'
+            })}
+          </div>
+        </div>
+      </Layout>
+    );
   }
 }
